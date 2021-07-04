f62eddaa44268bb67405913e577628d9eeacc8f4
pkshahid <62973330+pkshahid@users.noreply.github.com>
2021-07-04 10:37:19 +0530

    Add files via upload
    A simple Calulator code using switch case statement

---
+++ main.c
@@ -0,0 +1,35 @@
+// Simple Calculator Using C
+
+#include <stdio.h>
+int main() {
+  while(1){
+    char operator;
+    double first, second;
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+    
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &first, &second);
+
+
+    switch (operator) {
+      case '+':
+        printf("%.1lf + %.1lf = %.1lf\n", first, second, first + second);
+        break;
+      case '-':
+        printf("%.1lf - %.1lf = %.1lf\n", first, second, first - second);
+        break;
+      case '*':
+        printf("%.1lf * %.1lf = %.1lf\n", first, second, first * second);
+        break;
+      case '/':
+        printf("%.1lf / %.1lf = %.1lf\n", first, second, first / second);
+        break;
+      // operator doesn't match any case constant
+      default:
+        printf("Error! operator is not correct\n");
+    }
+  }
+
+  return 0;
+}
